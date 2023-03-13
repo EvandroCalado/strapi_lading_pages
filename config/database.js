838b43e0930dd10bd0e96e12c0dec4b88ea0aa7d
Evandro <evandrocalado07@gmail.com>
2023-03-13 09:04:36 -0300

    update

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'database'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true),
     },
     pool: {
       min: 0,
